1b5e1a2bc448c7cf211fb247cc28269882856990
Yoteichi <plonk@piano.email.ne.jp>
2017-05-20 20:40:56 +0900

    Dechunker をもうちょっとちゃんと実装した。

--- dechunker.rb
+++ dechunker.rb
@@ -12,9 +12,25 @@ class Dechunker
       return res
     else
       # read next chunk
-      line = @sock.gets.chomp
+      line = @sock.gets
+      if line.nil?
+        fail Errno::EPIPE, "failed to read chunk header"
+      end
+      line.chomp!
+      unless line =~ /\A[0-9A-Fa-f]+;?.*\z/
+        fail "invalid(?) chunk header #{line.inspect}"
+      end
       nbytes = line.to_i(16)
+      if nbytes == 0
+        until @sock.gets == "\r\n" # read trailer
+        end
+        fail Errno::EPIPE, "end of stream"
+      end
       @buffer.concat(@sock.read(nbytes))
+      crlf = @sock.read(2)
+      if crlf != "\r\n"
+        fail "protocol error"
+      end
       return read(n)
     end
   end
